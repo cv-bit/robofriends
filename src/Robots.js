d0cc7f1cc4bbe6cb83f9f51f90428a9e4fa9599e
cv-bit <ververcameron29@gmail.com>
2021-07-29 22:44:07 -0500

    first commit

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'c3po',
+        username: 'c3po',
+        email: 'c3po@april.biz'
+    },
+    {
+        id: 2,
+        name: 'r2d2',
+        username: 'r2d2',
+        email: 'r2d2@april.biz'
+    },
+    {
+        id: 3,
+        name: 'tron',
+        username: 'tron',
+        email: 'tron@april.biz'
+    },
+    {
+        id: 4,
+        name: 'wallee',
+        username: 'wallee',
+        email: 'wallee@april.biz'
+    },
+    {
+        id: 5,
+        name: 'baymax',
+        username: 'baymax',
+        email: 'baymax@april.biz'
+    },
+    {
+        id: 6,
+        name: 'megabot',
+        username: 'megabot',
+        email: 'megabot@april.biz'
+    },
+    {
+        id: 7,
+        name: 'optimus',
+        username: 'optimus',
+        email: 'optimus@april.biz'
+    },
+    {
+        id: 8,
+        name: 'megatron',
+        username: 'megatron',
+        email: 'megatron@april.biz'
+    },
+    {
+        id: 9,
+        name: 'starscreem',
+        username: 'starscreem',
+        email: 'starscreem@april.biz'
+    },
+    {
+        id: 10,
+        name: 'bumblebee',
+        username: 'bumblebee',
+        email: 'bumblebee@april.biz'
+    },
+]
